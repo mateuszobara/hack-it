643c1e30a0394c5fee54df0bcf841b7318213c15
Mateusz Obara <mateuszobara02@gmail.com>
2020-02-10 21:19:41 +0100

    Add page loader

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const app = document.querySelector('.app');
+
+const loadPage = (site) => {
+    const page = new XMLHttpRequest();
+    page.open('GET', `pages/${site}.html` , false);
+    page.send();
+
+    app.innerHTML = page.response;
+}
+loadPage('homepage');
+
 const hamburger = document.querySelector('.hamburger');
 const navigation = document.querySelector('.navigation');
 
@@ -6,9 +17,24 @@ hamburger.addEventListener('click', () => {
     navigation.classList.toggle('navigation--is-active');
 })
 
+const hideNavigation = () => {
+    hamburger.classList.remove('hamburger--is-active');
+    navigation.classList.remove('navigation--is-active');
+}
+
 const ctaButton = document.querySelector('.intro__button');
 const mainSection = document.querySelector('.main');
 
 ctaButton.addEventListener('click', () => {
     mainSection.scrollIntoView({behavior: 'smooth'});
+})
+
+const navigationLinks = [...document.querySelectorAll('.navigation__link')];
+
+navigationLinks.forEach((link) => {
+    link.addEventListener('click', (e) => {
+        let selectedItem = e.target.innerHTML;
+        loadPage(selectedItem);
+        hideNavigation();
+    });
 })
